a2e13372821f7ffa1b0ad365901a5bf3010cfd55
GabrielGrld <gbrlgrld@gmail.com>
2022-01-29 13:13:16 -0500

    refactor code to create functional component PorductsGrid that can be reused in other components

---
+++ src/components/ProductsGrid.js
@@ -0,0 +1,30 @@
+import Card from './Card';
+
+
+//Material UI components for responsive grid
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+
+
+export default function ProductsGrid({products}){
+
+
+   return(<Box sx={{ flexGrow: 1 }} >
+      <Grid container spacing={2} justify = "center"   >
+        {products && products.map((document) => (   
+        (<Grid  item xs={12} sm={6} md={4} lg ={3}>    
+            <div key={document.id} align="center">
+            <Card  
+          key = {document.imgUrl}
+          title={document.name}
+          description ={document.details.substring(0,100)}          
+          imgLink ={document.imgUrl}
+          item = {document} 
+          />
+            </div>
+            </Grid>)
+            ))}      
+
+      </Grid>
+    </Box>)
+}
--- src/pages/productscat/ProductCategory.js
+++ src/pages/productscat/ProductCategory.js
@@ -1,12 +1,43 @@
 import { useParams } from "react-router-dom"
+import { useCollection } from "../../hooks/useCollection"
+import ProductsGrid from '../../components/ProductsGrid'
 
 
 export default function ProductCategory() {
     const {category} = useParams()
+    const {documents, error} = useCollection('productos')
+    const currentFilter = category
     console.log(category)
+
+    const products = documents ? documents.filter((document)=>{
+        switch(currentFilter){
+          case 'all':
+            return true
+          case 'dama':
+              return document.category === 'calzado-dama'|'pantalon-dama'
+          case 'caballero':
+            return document.category === 'calzado-caballero'|'pantalon-caballero'|'camiseta-caballero'     
+          case 'calzado-caballero':
+          case 'calzado-dama':      
+          case 'pantalon-dama':
+          case 'pantalon-caballero':
+          case 'camiseta-caballero':        
+            return document.category === currentFilter
+          case 'vendido':
+            return document.sold ===true
+          case 'exhibido':
+            return document.sold ===false
+          default:
+            return true
+        }
+      }) : null
+      console.log(products)
     return (
-        <div>
-            <h1>Product Category {category}</h1>
+        <div className="container" style={{"margin":"1em", "alignItems": "center"}}>
+            <h1>Categoría: {category}</h1>
+            {error&&<p>{error}</p>}    
+           {products&&<ProductsGrid products={products} />}
+           {products&&products.length<1&&<p>Proximamente mas productos en esta categoria</p>}
         </div>
     )
 }
