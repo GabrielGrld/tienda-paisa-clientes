de2f5b54f05148d401521c57d5a36495c19a4c20
GabrielGrld <gbrlgrld@gmail.com>
2022-01-31 14:38:53 -0500

    refactor code, eliminate unnecessary  steps

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import { useCollection } from '../../hooks/useCollection'
 
-
-
 //Components
 import Carousel from '../../components/Carousel'
 import ProductsGrid from '../../components/ProductsGrid';
 
-
 //styles
 import './Home.css'
-
-
-
 
 function Home(){
   const {documents, error} = useCollection('productos')
@@ -21,31 +15,19 @@ function Home(){
   // if is done to wait the documents, since useCollection Hook fetchs the data, once the data is
   //ready, Filter is done to check what products are sold (true), or not sold (false). This will show 
   // in the pages only the productos that have not been sold (sold===false)
-  if(documents){
-    var products = documents.filter(product => product.sold===false)
-    
+  if(documents){    
+    var products = documents.filter(product => product.sold===false)    
   }
-
     return(
       
     <div className="container" style={{"margin":"1em"}}>
-    <Carousel />
-    
+    <Carousel />    
     <div>
     <h1>Productos Disponibles</h1>
     {error&&<p>{error}</p>}      
-    {products&&<ProductsGrid products={products} />}
-        
+    {products&&<ProductsGrid products={products} />}        
     </div>
-      
-      
-      
-      
-      
-      
-      
-      
-      </div>)
+    </div>)
 }
 
 
