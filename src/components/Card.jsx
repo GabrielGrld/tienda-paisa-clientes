ff4585d9ab32c620abe8242a7ece8eb72703b081
GabrielGrld <gbrlgrld@gmail.com>
2022-01-31 14:31:58 -0500

    add whatsapp link to cart button

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,6 +7,7 @@ import Typography from '@mui/material/Typography';
 import  CardActionArea  from '@mui/material/CardActionArea';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import PublicIcon from '@mui/icons-material/Public';
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 
 //Styles
 import  './Card.css';
@@ -23,6 +24,8 @@ var formatter = new Intl.NumberFormat('en-US', {
   //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
   //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
 });
+
+  const WAurl = `https://wa.me/573106426734?text=Hola%21%2c%20Estoy%20interesado%20en%20este%20producto%20https://paisa-store-ctg.web.app/producto/${item.id}`
 
 
   const price = parseInt(item.price)
@@ -50,16 +53,14 @@ var formatter = new Intl.NumberFormat('en-US', {
           <Typography variant="body2" color="text.secondary" className="card">
             Precio: {priceFormat}
           </Typography>
-          <Typography variant="body2" color="text.secondary" className="card">
-            ct: {item.ct}
-          </Typography>
+          
           </div>
         </CardContent>
       </CardActionArea>
       </Link>
-      <div className='buttons'>
-      <button className="comprar" onClick={()=>console.log("comprar activado")}> <ShoppingCartIcon /> </button>
-      <button className="comprar"> <PublicIcon /> </button>
+      <div className='buttons'>      
+      <button className="comprar" > <a target="_blank" href={WAurl}><ShoppingCartIcon /></a> </button>
+      <button className="comprar"> <Link  to={{pathname: `/producto/${item.id}`, query: {item}}}><InfoOutlinedIcon  /></Link> </button>
       </div>
     </Card>
     
